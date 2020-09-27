3dfef2ed7ec2c16f8265bea8e2d5b7562a3493dd
Anton Yurkevich <antonyurkevich@MacBook-Air-Anton.local>
2020-09-27 16:59:29 +0300

    Final

--- ClassWork/src/Saturday/SwitchClass2.java
+++ ClassWork/src/Saturday/SwitchClass2.java
@@ -31,11 +31,13 @@ public class SwitchClass2 {
             case 15: 
                 System.out.println("Nikita's commit");
 
-            case 15:
-                System.out.println("Anton case 15");
+            case 16:
+                System.out.println("Anton case 17");
+            case 17:
+                System.out.println("Anton case 17");
 
-           //
-
+           //sdfsdfsdfsdf
+                System.out.println("ssssss");
 
         }
         System.out.println("So let's finish the work");
